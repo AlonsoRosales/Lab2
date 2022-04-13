940d3efe273475bfc6593c5bd4bf680fcd7cb362
Alonso Rosales <a20191566@pucp.edu.pe>
2022-04-12 19:33:40 -0500

    sdes

--- src/main/java/com/example/lab2/Controllers/SedeController.java
+++ src/main/java/com/example/lab2/Controllers/SedeController.java
@@ -1,4 +1,33 @@
 package com.example.lab2.Controllers;
 
+
+import com.example.lab2.Entity.Sede;
+import com.example.lab2.Repository.SedeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/Sedes")
 public class SedeController {
+
+    @Autowired
+    SedeRepository sedeRepository;
+
+    @GetMapping("")
+    public String home(){
+        return "redirect:/Sedes/listarSedes";
+    }
+
+    @GetMapping("/listarSedes")
+    public String listarRegiones(Model model) {
+        List<Sede> listaSedes = sedeRepository.findAll();
+        model.addAttribute("listaSedes",listaSedes);
+        return "Sede/listaSedes";
+    }
+
 }
--- src/main/java/com/example/lab2/Repository/SedeRepository.java
+++ src/main/java/com/example/lab2/Repository/SedeRepository.java
@@ -1,4 +1,9 @@
 package com.example.lab2.Repository;
 
-public interface SedeRepository {
+import com.example.lab2.Entity.Sede;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+public interface SedeRepository extends JpaRepository<Sede, Integer> {
 }
