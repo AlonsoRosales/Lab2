5b880ed7a2e3fc3049cd1474fbc72adab52f342d
Diego Vargas <a20191027@pucp.edu.pe>
2022-04-12 19:25:46 -0500

    Marcas entity y repository

--- src/main/java/com/example/lab2/Entity/Marcas.java
+++ src/main/java/com/example/lab2/Entity/Marcas.java
@@ -1,10 +1,32 @@
 package com.example.lab2.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="marcas")
 public class Marcas {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idmarca", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", length = 45)
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/com/example/lab2/Repository/MarcasRepository.java
@@ -0,0 +1,7 @@
+package com.example.lab2.Repository;
+
+import com.example.lab2.Entity.Marcas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcasRepository extends JpaRepository<Marcas,Integer> {
+}
