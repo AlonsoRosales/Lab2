7c155e407d2467f9d83b17d517b9bffd2f33a637
Diego Vargas <a20191027@pucp.edu.pe>
2022-04-12 20:31:31 -0500

    Marcas edit y new

--- src/main/java/com/example/lab2/Controllers/MarcasController.java
+++ src/main/java/com/example/lab2/Controllers/MarcasController.java
@@ -1,11 +1,17 @@
 package com.example.lab2.Controllers;
 
+import com.example.lab2.Entity.Marcas;
 import com.example.lab2.Repository.MarcasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/marcas")
@@ -19,4 +25,48 @@ public class MarcasController {
         model.addAttribute("listaMarcas",marcasRepository.findAll());
         return "marcas/list";
     }
+
+
+    @PostMapping("save")
+    public String guardarMarca(Marcas marcas,RedirectAttributes attr) {
+        if (marcas.getId() == 0) {
+            attr.addFlashAttribute("msg", "Marca creada exitosamente");
+        } else {
+            attr.addFlashAttribute("msg", "Marca actualizada exitosamente");
+        }
+        marcasRepository.save(marcas);
+        return "redirect:/marcas";
+    }
+
+    @GetMapping("/new")
+    public String newMarcaForm(){
+        return "marcas/newFrm";
+    }
+
+
+    @GetMapping("edit")
+    public String editarMarcas(Model model,@RequestParam("id") Integer id) {
+        Optional<Marcas> optMarca = marcasRepository.findById(id);
+        if(optMarca.isPresent()){
+            Marcas marca = optMarca.get();
+            model.addAttribute("marca",marca);
+            return "marcas/editFrm";
+        }else  {
+            return "redirect:/marcas";
+        }
+    }
+
+    @PostMapping("delete")
+    public String borrarMarcas(Model model, @RequestParam("id") int id, RedirectAttributes attr) {
+        Optional<Marcas> optMarca = marcasRepository.findById(id);
+        if(optMarca.isPresent()){
+            marcasRepository.deleteById(id);
+            attr.addFlashAttribute("msg","Marca borrada exitosamente");
+        }
+         return "redirect:/marcas";
+    }
+
+
+
+
 }
